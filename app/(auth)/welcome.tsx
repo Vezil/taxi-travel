baaa6731f7d988fbf434c995587540edb4aedd2b
Vezil <szymonwojaczek@wp.pl>
2024-09-09 18:00:14 +0200

    add swiper and consts

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,39 @@
-import { Text } from 'react-native';
+import { useRef, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Swiper from 'react-native-swiper';
+import { useRouter } from 'expo-router';
+import { onboarding } from '@/constants';
 
 const Welcome = () => {
+    const router = useRouter();
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
     return (
-        <SafeAreaView>
-            <Text>Welcome</Text>
+        <SafeAreaView className="flex h-full items-center justify-between">
+            <TouchableOpacity
+                onPress={() => {
+                    router.replace('/(auth)/sign-up');
+                }}
+                className="w-full flex justify-end items-end p-5"
+            >
+                <Text className="text-black text-md font-JakartaBold">Skip</Text>
+            </TouchableOpacity>
+
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={<View className="w-8 h-1 bg-[#E2E8F0] rounded-full" />}
+                activeDot={<View className="w-8 h-1 bg-[#0286FF] rounded-full" />}
+                onIndexChanged={index => setActiveIndex(index)}
+            >
+                {onboarding.map(item => (
+                    <View>
+                        <Text>{item.title}</Text>
+                    </View>
+                ))}
+            </Swiper>
         </SafeAreaView>
     );
 };
---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const onboarding = [
+    {
+        id: 1,
+        title: 'The perfect ride is just a tap away! 1',
+        description: 'The journey begins with Ryde',
+        image: null
+    },
+    {
+        id: 2,
+        title: 'The perfect ride is just a tap away! 2',
+        description: 'The journey begins with Ryde',
+        image: null
+    },
+    { id: 3, title: 'The perfect ride is just a tap away! 3', description: 'The journey begins with Ryde', image: null }
+];
