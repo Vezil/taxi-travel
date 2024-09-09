0ee6200f5814904da8b6bb76c33b25f9419445c1
Vezil <szymonwojaczek@wp.pl>
2024-09-09 17:31:36 +0200

    add folders structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,7 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Redirect } from 'expo-router';
 
 const HomePage = () => {
-    return (
-        <SafeAreaView>
-            <Text>Home Page (Taxi Travel)</Text>
-        </SafeAreaView>
-    );
+    return <Redirect href="/(auth)/welcome" />;
 };
 
 export default HomePage;
