ee0b4a5c9c61bcc018f2ac43adffab6d7d864a4c
Vezil <szymonwojaczek@wp.pl>
2024-09-17 21:35:15 +0200

    fix view structure

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -17,8 +17,6 @@ const InputField = ({
             <TouchableWithoutFeedback>
                 <View className="my-2 w-full">
                     <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>{label}</Text>
-                </View>
-                <View>
                     <View
                         className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border
                  border-neutral-100 focus:border-primary-500 ${containerStyle}`}
